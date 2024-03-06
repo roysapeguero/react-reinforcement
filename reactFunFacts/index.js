07b5cd7d6c520509b60f838e0c67370b83d84fc7
roysa <roysapeguero@outlook.com>
2024-03-06 15:37:38 -0800

    complete part 1 of project 1

--- reactFunFacts/index.js
+++ reactFunFacts/index.js
@@ -1,13 +1,21 @@
-const navbar = (
-  <nav>
-    <h1>website</h1>
+/*
+Challenge: Starting from scratch, build and render the
+HTML for our section project.
+*/
+
+const funFacts = (
+  <div>
+    <img width="200" src="react-logo.jpg" />
+    <h1>Fun facts about React</h1>
     <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
-  </nav>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(navbar);
+root.render(funFacts);
