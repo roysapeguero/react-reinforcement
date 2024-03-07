ee1ec6b44a43fa04ee07893f896465787d51d39f
roysa <roysapeguero@outlook.com>
2024-03-06 17:46:21 -0800

    add some styling

--- reactFunFacts/index.js
+++ reactFunFacts/index.js
@@ -25,11 +25,27 @@ Challenge:
   called "MainContent" and render inside Page as well.
 */
 
+/**
+Challenge:
+
+- Add an `ul` inside the Header's `nav` and create
+  the following `li`s: "Pricing", "About", & "Contact"
+- Using flexbox, line up the nav items horizontally, and
+  put them inline with the React logo.
+- Change the image styling to happen in CSS instead of in-line
+  For practice, add a new class to the image in order to style it
+*/
+
 function Header() {
   return (
     <header>
-      <nav>
-        <img src="./react-logo.png" width="100px" alt="React Logo" />
+      <nav className="nav">
+        <img className="nav-logo" src="./react-logo.png" alt="React Logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </nav>
     </header>
   );
@@ -37,7 +53,7 @@ function Header() {
 
 function MainContent() {
   return (
-    <div>
+    <div className="main-content">
       <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>Popular library</li>
@@ -49,12 +65,16 @@ function MainContent() {
 }
 
 function Footer() {
-  return <footer>© 2024 Peguero development. All rights reserved.</footer>;
+  return (
+    <footer className="footer">
+      <small>© 2024 Peguero development. All rights reserved.</small>
+    </footer>
+  );
 }
 
 function Page() {
   return (
-    <div>
+    <div className="page-container">
       <Header />
       <MainContent />
       <Footer />
