f59d275ae2d03fc860c9a499de6502cd1a8df391
roysa <roysapeguero@outlook.com>
2024-05-22 13:49:18 -0700

    about content and styling

--- digital-business-card/src/components/About.jsx
+++ digital-business-card/src/components/About.jsx
@@ -1,7 +1,12 @@
 export default function About() {
   return (
-    <>
-      <h1>About</h1>
-    </>
+    <div className="about">
+      <h3 className="about--title">About Me</h3>
+      <p className="about--body">
+        I&#39;m a full stack developer and student based in the Greater Phoenix
+        Area, Arizona. I thrive on continuous learning and am always eager to
+        explore something new!
+      </p>
+    </div>
   );
 }
