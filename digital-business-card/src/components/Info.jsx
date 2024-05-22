a05958d21ba330deaba328546a12c7ae210fbf60
roysa <roysapeguero@outlook.com>
2024-05-22 12:21:04 -0700

    info section styling

--- digital-business-card/src/components/Info.jsx
+++ digital-business-card/src/components/Info.jsx
@@ -1,7 +1,26 @@
+import profilePicture from "../assets/me.jpg";
+import mailIcon from "../assets/mail.png";
+import linkedinIcon from "../assets/linkedin.png";
+
 export default function Info() {
   return (
-    <>
-      <h1>Info</h1>
-    </>
+    <header>
+      <img className="info--profile_picture" src={profilePicture} />
+      <div className="info--personal_info">
+        <h1 className="info--name">Roysa Peguero Martinez</h1>
+        <h3 className="info--position">Full Stack Developer</h3>
+        <p className="info--website">roysapeguero.com</p>
+      </div>
+      <div className="info--contact">
+        <button className="info--contact_button email">
+          {" "}
+          <img className="info--button_icon" src={mailIcon} /> Email
+        </button>
+        <button className="info--contact_button linkedin">
+          <img className="info--button_icon" src={linkedinIcon} />
+          LinkedIn
+        </button>
+      </div>
+    </header>
   );
 }
