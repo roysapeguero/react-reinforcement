2d59b482de1d8f2a2395b28cc886c483e07b8d5b
roysa <roysapeguero@outlook.com>
2024-05-22 14:53:23 -0700

    add footer

--- digital-business-card/src/components/Footer.jsx
+++ digital-business-card/src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import githubIcon from "../assets/github.png";
+
 export default function Footer() {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <div className="footer">
+      <a target="_blank" href="https://www.github.com/roysapeguero">
+        <img className="footer--github_icon" src={githubIcon} />
+      </a>
+    </div>
   );
 }
