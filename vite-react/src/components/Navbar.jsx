b158f9c242d8e64f0d32416560c8c9fa33db9100
roysa <roysapeguero@outlook.com>
2024-05-22 09:15:35 -0700

    navbar styling

--- vite-react/src/components/Navbar.jsx
+++ vite-react/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import logo from "../assets/react.svg";
+
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav>
+      <img className="react-logo" src={logo} />
+      <h3 className="title">ReactFacts</h3>
+      <h4 className="subtitle">React Course - Project 1</h4>
+    </nav>
   );
 }
