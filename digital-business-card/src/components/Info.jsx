6f9ace527544af063336c1e404afe80fe1d76efc
roysa <roysapeguero@outlook.com>
2024-05-22 13:25:14 -0700

    add button functionality and styling

--- digital-business-card/src/components/Info.jsx
+++ digital-business-card/src/components/Info.jsx
@@ -12,14 +12,17 @@ export default function Info() {
         <p className="info--website">roysapeguero.com</p>
       </div>
       <div className="info--contact">
-        <button className="info--contact_button email">
-          {" "}
-          <img className="info--button_icon" src={mailIcon} /> Email
-        </button>
-        <button className="info--contact_button linkedin">
-          <img className="info--button_icon" src={linkedinIcon} />
-          LinkedIn
-        </button>
+        <a target="_blank" href="mailto:rypeguer@asu.edu">
+          <button className="info--contact_button email">
+            <img className="info--button_icon" src={mailIcon} /> Email
+          </button>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/roysapeguero">
+          <button className="info--contact_button linkedin">
+            <img className="info--button_icon" src={linkedinIcon} />
+            LinkedIn
+          </button>
+        </a>
       </div>
     </header>
   );
