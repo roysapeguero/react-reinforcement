1d9011efebc39d7d48b7e9dd338e3bf68277c2d8
roysa <roysapeguero@outlook.com>
2024-05-22 09:42:21 -0700

    body styling

--- vite-react/src/components/Main.jsx
+++ vite-react/src/components/Main.jsx
@@ -1,7 +1,18 @@
 export default function Main() {
   return (
-    <div>
-      <h1>Main</h1>
-    </div>
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--list">
+        <li className="main--list_item">Was first released in 2013</li>
+        <li className="main--list_item">
+          Was originally created by Jordan Walke
+        </li>
+        <li className="main--list_item">Has well over 100K stars on GitHub</li>
+        <li className="main--list_item">Is maintained by Facebook</li>
+        <li className="main--list_item">
+          Powers thousands of enterprise apps, including mobile apps
+        </li>
+      </ul>
+    </main>
   );
 }
--- vite-react/src/components/Navbar.jsx
+++ vite-react/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import logo from "../assets/react.svg";
 export default function Navbar() {
   return (
     <nav>
-      <img className="react-logo" src={logo} />
-      <h3 className="title">ReactFacts</h3>
-      <h4 className="subtitle">React Course - Project 1</h4>
+      <img className="nav--react_logo" src={logo} />
+      <h3 className="nav--title">ReactFacts</h3>
+      <h4 className="nav--subtitle">React Course - Project 1</h4>
     </nav>
   );
 }
