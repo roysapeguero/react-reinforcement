68597d0f46402697e2a2b6894259ac44e1bd35a2
roysa <roysapeguero@outlook.com>
2024-05-28 23:52:04 -0700

    component display

--- airbnb-experiences-clone/src/App.jsx
+++ airbnb-experiences-clone/src/App.jsx
@@ -1,3 +1,11 @@
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+
 export default function App() {
-  return <>hi</>;
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
 }
