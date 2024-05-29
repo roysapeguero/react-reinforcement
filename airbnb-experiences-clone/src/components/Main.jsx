1aa05ae690122df357c94ef9a35d155200bf60d7
roysa <roysapeguero@outlook.com>
2024-05-29 07:14:26 -0700

    main styling

--- airbnb-experiences-clone/src/components/Main.jsx
+++ airbnb-experiences-clone/src/components/Main.jsx
@@ -1,3 +1,16 @@
+import hero from "../assets/hero.png";
+
 export default function Main() {
-  return <main>Main</main>;
+  return (
+    <main>
+      <img className="airbnb--hero" src={hero} alt="" />
+      <div className="airbnb--body_section">
+        <h1 className="airbnb--title">Online Experiences</h1>
+        <p className="airbnb--body_text">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </main>
+  );
 }
